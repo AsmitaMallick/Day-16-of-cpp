92885907d9f758f086ff512838b82ea118e3e3b8
ASMITA MALLICK <asmitamallick2@gmail.com>
2023-03-18 23:49:53 +0530

    cpp

---
+++ dukan, cpp 1 day 16.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Shop
+{
+	int itemId[100];
+	int itemPrice[100];
+	int counter;
+	public:
+		void initCounter(void){counter = 0
+		;}
+		void setPrice(void);
+		void displayPrice(void);
+		
+};
+void Shop :: setPrice(void){
+	cout<<"enter id of your item number: "<<counter +1<<endl;
+	cin>>itemId[counter];
+	cout<<"enter price of your item: "<<counter +1<<endl;
+	cin>>itemPrice[counter];
+	counter++;
+}
+void Shop ::displayPrice(void){
+	for(int i=0; i<counter; i++){
+		cout<<"the price of item with id: "<<itemId[i]<<" is "<<itemPrice[i]<<endl;
+	}
+}
+int main()
+{
+	Shop dukan;
+	dukan.initCounter();
+	dukan.setPrice();
+	dukan.setPrice();
+	dukan.setPrice();
+	dukan.displayPrice();
+	return 0;
+}
